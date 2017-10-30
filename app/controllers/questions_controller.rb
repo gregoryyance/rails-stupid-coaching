372e070f16197e6cb38a6401f29e8dfea0c02e1c
Gregory Yance <gregoryyance@gmail.com>
2017-10-30 14:22:39 +0000

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def answer
+    @query = params[:query]
+    @answer = coach_answer(@query)
+  end
+  def ask
+  end
+
+  def coach_answer(your_message)
+    if your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+      elsif your_message == "I am going to work right now!"
+        return ""
+      else 
+        return "I don't care, get dressed and go to work!"
+      end
+  end
+end
